4385c199b79cffe4a516488ba1e6f47c1bbab376
Scott Maxwell <scott@codecobblers.com>
2014-09-03 21:28:28 -0700

    Add flushes to write_three_lines.py

--- tests/executables/write_three_lines.py
+++ tests/executables/write_three_lines.py
@@ -3,25 +3,15 @@ from time import sleep
 
 __author__ = 'Scott Maxwell'
 
-# with open('aack0.txt', 'w') as f:
-#     f.write('ran')
-
 print('Version: %s' % sys.version.partition(' ')[0])
-# sys.stdout.flush()
+sys.stdout.flush()
 sleep(.1)
-# with open('aack1.txt', 'w') as f:
-#     f.write('ran')
 
 print('Executable: %s' % sys.executable)
-# sys.stdout.flush()
+sys.stdout.flush()
 sleep(.1)
-# with open('aack2.txt', 'w') as f:
-#     f.write('ran')
 
 print('Args: %s' % ' '.join(sys.argv[1:]))
-sys.stderr.write('done')
-# with open('aack3.txt', 'w') as f:
-#     f.write('ran')
+sys.stdout.flush()
 
-# with open('aack.txt', 'w') as f:
-#     f.write('ran')
+sys.stderr.write('done')
