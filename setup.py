b98b6d0c30b91bee5ee63d4a460a7bff4fb47fef
Scott Maxwell <scott@codecobblers.com>
2014-11-06 13:16:31 -0800

    Bump version to 1.0.0

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ except ImportError:
 
 setup(
     name="papa",
-    version="0.9.5",
+    version="1.0.0",
     packages=["papa", "papa.server", "tests"],
     author="Scott Maxwell",
     author_email="scott@codecobblers.com",
